f5735575ee3253945e36ed887f8782526048a0da
rubacodes <rubacodes@gmail.com>
2022-05-10 15:42:17 +0200

    importato script precedente

---
+++ superbonus/script.js
@@ -0,0 +1,105 @@
+
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
+// --- FUNZIONI ---
+//funzione che genera il campo di battaglia con un numero di tile pari a quello della difficolta
+function fieldGenerator(tileNumber, hook) {
+    const father = document.querySelector(hook);
+    for (let i = 1; i <= tileNumber; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.id = i;
+        box.append(i);
+        father.append(box);
+    }
+}
+
+// genera numero random compreso da min a max
+function randomNumberRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// funzione che genera N bombe: ritorna un array di n numeri univoci nel range specificato
+function generateBombs(bombNumber, range) {
+    const array = [];
+    while (array.length < bombNumber) {
+        const number = randomNumberRange(1, range);
+        if (!array.includes(number)) {
+            array.push(number);
+        }
+    }
+    return array;
+}
+//funzione che ritorna il range massimo associato alal difficolta del gioco
+function difficultySelect(diff) {
+    let maxRange;
+    if (diff === 0) {
+        return maxRange = 100;
+    } else if (diff === 1) {
+        return maxRange = 80;
+    } else {
+        return maxRange = 50;
+    }
+}
+// --- MAIN ---
+//--aggiungere modale al termine del gioco
+//const modale = document.getElementsByClassName('modal')[0];
+//const risultato = document.getElementById('risultato');
+let difficulty;
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+do {
+    difficulty = Number(prompt(`Scegliere la difficolta': 0 => tra 1 e 100 , 1 => tra 1 e 80 , 2 => tra 1 e 50`))
+} while (isNaN(difficulty) || difficulty < 0 || difficulty > 2);
+
+let maxDifficultyRange = difficultySelect(difficulty);
+fieldGenerator(maxDifficultyRange, '.field');
+// Il computer deve generare 16 numeri casuali tra 1 e il range specificato dalla difficolta'.
+// I numeri non possono essere duplicati.
+const bombs = generateBombs(16, maxDifficultyRange);
+console.log(bombs);
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta
+
+let userNumber; //scelta corrente del giocatore
+const userPickedNumbers = [];// numeri scelti dal giocatore, la lunghezza della lista sara' il punteggio  finale del player 
+do {
+    do {
+        userNumber = Number(prompt('Inserisci un numero tra quelli rimanenti:'))
+    } while (isNaN(userNumber) || userNumber < 1 || userNumber > maxDifficultyRange); //sempre compreso tra 1 e e il range di difficolta selezionato.
+
+    //se in numero non e' mai stato scelto aggiungilo alla lista( L’utente non può inserire più volte lo stesso numero.)
+    if (!userPickedNumbers.includes(userNumber)) {
+        userPickedNumbers.push(userNumber);
+        console.log(userPickedNumbers);
+    }
+    document.getElementById(`${userNumber}`).className = 'box clear' // colora di verde il box pulito
+    // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+    if ((maxDifficultyRange - bombs.length) == userPickedNumbers.length) {
+        risultato.append(`Hai vinto e il tuo punteggio e' ${userPickedNumbers.length}`);//per la modale
+        console.log(`Hai vinto e il tuo punteggio e' ${userPickedNumbers.length}`);//per la console
+    }
+    // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.    
+} while (!bombs.includes(userNumber) && userPickedNumbers.includes(userNumber)); // chiedi numero finche non e' gia stato scelto o se ha preso una bomba
+
+// La partita termina quando il giocatore inserisce un numero “vietato” / colora di rosso la bomba
+console.log(`Hai colpito una bomba e il tuo punteggio e' ${userPickedNumbers.length - 1}`); //tolgo dal punteggio l'ultima immissione , per la console
+document.getElementById(`${userNumber}`).className = 'box bomb'
+
+//- al termine della partita mostra le bombe 
+for (let i = 0; i < bombs.length; i++) {
+    document.getElementById(`${bombs[i]}`).className = 'box bomb';
+}
+
+
+risultato.append(`Hai colpito una bomba e il tuo punteggio e' ${userPickedNumbers.length - 1}`);//per la modale
+modale.classList.add('active');
+//da implelementare
+//- clicca qui per continuare tra un prompt e l'altro per permettere al giocatore di vedere i numeri rimasti da selezionare
+
+//--aggiungere modale al termine del gioco
+
